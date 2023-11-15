ea2b82af97a695fb76715a0ce1a843eb67f8df7c
Rishabh Goel <rgoel22@umd.edu>
2023-11-15 17:46:11 -0500

    Project Setup

---
+++ src/main/java/com/pathshala/PathShalaApplication.java
@@ -0,0 +1,13 @@
+package com.pathshala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathShalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathShalaApplication.class, args);
+    }
+
+}

fbfefa4118ba52d6f0e2985e2900ad8345b01082
Rishabh Goel <rgoel22@umd.edu>
2023-12-01 22:54:14 -0500

    cors

--- src/main/java/com/pathshala/PathShalaApplication.java
+++ src/main/java/com/pathshala/PathShalaApplication.java
@@ -3,11 +3,13 @@ package com.pathshala;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
 @EnableJdbcRepositories
 @EntityScan("com.pathshala.dao")
+@ComponentScan("com.pathshala.config")
 public class PathShalaApplication {
 
     public static void main(String[] args) {

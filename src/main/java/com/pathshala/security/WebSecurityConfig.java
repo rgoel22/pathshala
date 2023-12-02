d77600f1bce9bf95b93819c60bbdb8a3c91616ac
aashrutpant <apant12@umd.edu>
2023-12-01 22:42:49 -0500

    cors isssue

--- src/main/java/com/pathshala/security/WebSecurityConfig.java
+++ src/main/java/com/pathshala/security/WebSecurityConfig.java
@@ -1,67 +1,25 @@
-/*
+
 package com.pathshala.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-*/
-/*@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)*//*
-
-public class WebSecurityConfig extends WebSecurityConfiguration {
-
-  @Value("${pam.dev.username}")
-  private String devUsername;
-
-  @Value("${pam.dev.password}")
-  private String devPassword;
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-
-  // @formatter:off
-  */
-/*@Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()*//*
-*/
-/*
-      .csrf().disable()
-      .cors().disable()
-      .httpBasic()
-        .and()
-      .authorizeRequests()
-      .anyRequest()
-        .permitAll()*//*
-*/
-/*
-    ;
-  }
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/logout", "/login");
-  }*//*
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception  {
-    auth
-      .inMemoryAuthentication()
-        .withUser(devUsername)
-          .password(devPassword)
-          .roles("DEVELOPER")
-    ;
-  }
-  //@formatter:on
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/pathshala/**")
+                .allowedOrigins("https://pathshala-adee99131d70.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+
 
 }
-*/
+

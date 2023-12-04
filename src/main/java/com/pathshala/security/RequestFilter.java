b70aad920d146fbf2feb6eb37c7f0a7e2a194f15
Rishabh Goel <rgoel22@umd.edu>
2023-12-03 20:52:09 -0500

    Enabled Token

--- src/main/java/com/pathshala/security/RequestFilter.java
+++ src/main/java/com/pathshala/security/RequestFilter.java
@@ -7,14 +7,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.apache.catalina.connector.RequestFacade;
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -24,40 +23,37 @@ public class RequestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException, BaseRuntimeException {
-//        if(((RequestFacade) request).getMethod().equalsIgnoreCase("OPTIONS")){
-//            response.setContentType("text/plain");
-//            response.setCharacterEncoding("UTF-8");
-//            response.getWriter().write("OK");
-//            response.setHeader("Access-Control-Allow-Origin", "*");
-//            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//            response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
-//            response.getWriter().flush();
-//            response.getWriter().close();
-//
-//        } else{
+        if (((RequestFacade) request).getMethod().equalsIgnoreCase("OPTIONS")) {
+            response.setContentType("text/plain");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("OK");
+            ((HttpServletResponse) response).setHeader("Access-Control-Allow-Origin", "*");
+            ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "*");
+            response.getWriter().flush();
+            response.getWriter().close();
+        } else {
             String url = ((RequestFacade) request).getRequestURL().toString();
             tokenService.expireToken();
-            if(false){
-           // if( !url.contains("login") && !url.contains("signUp")){
-                String payload = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-                JSONObject jsonObject = new JSONObject(payload);
-                Long userId = jsonObject.optLongObject("loogedInUserId", 0L);
-                String token = jsonObject.optString("token", "");
-                String userType = jsonObject.optString("userType", "");
-                if (tokenService.validateToken(userId, token, userType)) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            //  if(false){
+            if (!url.contains("login") && !url.contains("signUp")) {
+                String token = httpRequest.getHeader("authorization-token");
+                String userId = httpRequest.getHeader("userId");
+                String userType = httpRequest.getHeader("userType");
+                if (tokenService.validateToken(Long.parseLong(userId), token, userType)) {
                     filterChain.doFilter(request, response);
                 } else {
                     throw new UnauthorizedAccessException("Hello", "Hello");
                 }
-            } else{
+            } else {
 
                 ((HttpServletResponse) response).setHeader("Access-Control-Allow-Origin", "*");
                 ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
-
+                ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "*");
                 filterChain.doFilter(request, response);
             }
-
+        }
     }
 
 }
